01b61d2e31dbb360266475fac7bd67a55f4ce3f2
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-23 16:03:18 -0400

    prod release
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/app.tf
+++ terraform/app.tf
@@ -18,22 +18,28 @@ resource "cloudflare_record" "app" {
 resource "cloudflare_pages_project" "app" {
   account_id        = var.cloudflare_account_id
   name              = var.project_name
-  production_branch = "gh-pages"
+  production_branch = "prod-content"
   source {
     type = "github"
     config {
       owner                         = "dotcomrow"
       repo_name                     = var.project_name
-      production_branch             = "gh-pages"
+      production_branch             = "prod-content"
       pr_comments_enabled           = true
       deployments_enabled           = true
       production_deployment_enabled = true
-      preview_deployment_setting    = "none"
+      preview_deployment_setting    = "custom"
+      preview_branch_includes       = ["dev"]
+      preview_branch_excludes       = ["master", "prod"]
     }
   }
 
   deployment_configs {
     production {
+
+    }
+
+    preview {
 
     }
   }
