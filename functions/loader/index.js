12db7758f69c516a3d6ea6afe0939970f1a1479a
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-26 17:15:14 -0400

    test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- functions/loader/index.js
+++ functions/loader/index.js
@@ -1,5 +1,9 @@
-export default {
-    async fetch(request, env, context) {
-        return new Response("Hello world");
+export const onRequest = async (context) => {
+    const url = new URL(context.request.url)
+    // if homepage
+    if (url.pathname === "/") {
+        const asset = await context.env.ASSETS.fetch(url)
+        return context.env.ASSETS.fetch(url)
     }
+    return context.next()
 }
