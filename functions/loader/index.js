8ea2855b96b74433e4c0749f1819b47eb73a2048
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-26 18:44:15 -0400

    another test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- functions/loader/index.js
+++ functions/loader/index.js
@@ -1,15 +1,9 @@
 export const onRequest = async (context) => {
-    const url = new URL(context.request.url)
-    // if homepage
-    if (fileExtension(url.pathname) === 'html') {
-        const asset = await context.env.ASSETS.fetch(url)
-        // fetch config and inject
-        console.log("handled html")
-        return context.env.ASSETS.fetch(url)
-    }
-    return context.next()
-}
+  const url = new URL(context.request.url);
+  // if homepage
 
-var fileExtension = function( url ) {
-    return url.split('.').pop().split(/\#|\?/)[0];
-}
+  const asset = await context.env.ASSETS.fetch(url);
+  // fetch config and inject
+  console.log("handled html");
+  return context.env.ASSETS.fetch(url);
+};
