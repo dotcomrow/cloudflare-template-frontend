72dfd2702c5683bd87544a0f4ae95524835e363b
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-27 09:38:51 -0400

    hmm
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ export const onRequest = async (context) => {
   const asset = await context.env.ASSETS.fetch(url);
   var body = await asset.text();
 
+  console.log(context.env)
   body = body.replace(/{\"config\":\"config\"}/g, JSON.stringify(context.env.CONFIGS));
 
   return new Response(body, asset);
