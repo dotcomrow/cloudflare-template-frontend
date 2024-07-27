ce6b86dfe9e9e4dd2e3d7b91f4fe9db1ba8d14bd
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-07-27 08:15:07 -0400

    cleanup
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,9 @@
-import { GCPLogger } from "npm-gcp-logging";
-import { GCPAccessToken } from "npm-gcp-token";
-
 export const onRequest = async (context) => {
   const url = new URL(context.request.url);
-  // if homepage
   const asset = await context.env.ASSETS.fetch(url);
   var body = await asset.text();
 
-  console.log(context.env)
-  console.log(JSON.stringify(context.env.CONFIGS));
   body = body.replace(/{\"config\":\"config\"}/g, JSON.stringify(context.env.CONFIGS));
-  // fetch config and inject
-  // var logging_token = await new GCPAccessToken(
-  //   context.env.GCP_LOGGING_CREDENTIALS
-  // ).getAccessToken("https://www.googleapis.com/auth/logging.write");
-  // await GCPLogger.logEntry(
-  //   context.env.GCP_LOGGING_PROJECT_ID,
-  //   logging_token.access_token,
-  //   context.env.LOG_NAME,
-  //   [
-  //     {
-  //       severity: "INFO",
-  //       // textPayload: message,
-  //       jsonPayload: {
-  //         asset: asset,
-  //       },
-  //     },
-  //   ]
-  // );
+
   return new Response(body, asset);
 };
