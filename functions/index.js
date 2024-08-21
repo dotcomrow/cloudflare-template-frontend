d26fa457092b5ddaba0f319ed4b7dbdeb424a647
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-08-20 20:01:47 -0400

    fix
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,10 @@ const config_include_list = ["apiBase"];
 
 export const onRequest = async (context) => {
 
+  const url1 = new URL(context.request.url);
+  const asset1 = await context.env.ASSETS.fetch(url1);
+  var body = await asset1.text();
+
   body = body.replace(
     /{\"config\":\"config\"}/g,
     JSON.stringify(response_configs)
@@ -16,7 +20,7 @@ export const onRequest = async (context) => {
 
   return new Response(body, asset);
 
-  
+
   const url = new URL(context.request.url);
   const asset = await context.env.ASSETS.fetch(url);
   var body = await asset.text();
